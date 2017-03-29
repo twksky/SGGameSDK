960b89dc5f73528347bf953b81c699b36a6a9b2e
twksky <364116799@qq.com>
2017-03-29 21:30:24 +0800

    xib

---
+++ GameSDKTest/PayViewController.h
@@ -0,0 +1,13 @@
+//
+//  PayViewController.h
+//  GameSDKTest
+//
+//  Created by twksky on 2017/3/28.
+//  Copyright © 2017年 twksky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PayViewController : UIViewController
+
+@end
---
+++ GameSDKTest/PayViewController.m
@@ -0,0 +1,41 @@
+//
+//  PayViewController.m
+//  GameSDKTest
+//
+//  Created by twksky on 2017/3/28.
+//  Copyright © 2017年 twksky. All rights reserved.
+//
+
+#import "PayViewController.h"
+
+@interface PayViewController ()
+
+@end
+
+@implementation PayViewController
+
+-(UIInterfaceOrientationMask)supportedInterfaceOrientations{
+    return UIInterfaceOrientationMaskLandscape;//左右横屏
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
