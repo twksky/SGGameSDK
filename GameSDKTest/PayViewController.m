d5649825de2b94bfe4b204799f44edb76f5d3208
twksky <364116799@qq.com>
2017-03-30 12:29:12 +0800

    complete

--- GameSDKTest/PayViewController.m
+++ GameSDKTest/PayViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "PayViewController.h"
+#import "GameSDK.h"
+#import <ZWXPaySDK/ZWXPaySDK.h>
 
 @interface PayViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *zifeiLabel;
@@ -26,7 +28,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    _payNum = 0.1;
+    _payNum = 1;
     self.zifeiLabel.text = [NSString stringWithFormat:@"%.2lf元",_payNum];
     self.yingfujineLabel.text = [NSString stringWithFormat:@"%.2lf元",_payNum];
     
@@ -37,10 +39,40 @@
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)wxPayAction:(id)sender {
-    
+    [GameSDK gameSDK_WXPayWithBundleName:[NSString stringWithFormat:@"twk%zd",arc4random()] withUserId:[NSString stringWithFormat:@"twkID%zd",arc4random()] withGameBillId:[NSString stringWithFormat:@"twkBill%zd",arc4random()] withGameCode:6 withAmt:_payNum withSuccusBlock:^(NSString *url,NSString *prepayId) {
+        
+        if (![[ZWXPaySDK defaultService] isWXAppInstalled]) {
+            //没有安装微信
+            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:url]];
+            
+        }else{
+            //安装微信
+            ZWXPayReqObject *payReqObj = [[ZWXPayReqObject alloc] init];
+            payReqObj.prepayUrl = url;
+            payReqObj.prepayId = prepayId;
+            [[ZWXPaySDK defaultService] sendPayReq:payReqObj viewController:self completeBlock:^(ZWXPayRespObject *respObject) {
+                
+                if (respObject.status) {
+                    NSLog(@"成功");
+                } else {
+                    NSLog(@"失败");
+                }
+            }];
+        }
+    }withFaildBlock:^(NSString *errorMsg) {
+        NSLog(@"%@",errorMsg);
+    }];
 }
+
 - (IBAction)aliPayAction:(id)sender {
-    
+    [GameSDK gameSDK_AliPayWithBundleName:[NSString stringWithFormat:@"twk%zd",arc4random()] withUserId:[NSString stringWithFormat:@"twkID%zd",arc4random()] withGameBillId:[NSString stringWithFormat:@"twkBill%zd",arc4random()] withGameCode:6 withAmt:_payNum withSuccusBlock:^(NSString *url) {
+        
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:url]];
+        
+    } withFaildBlock:^(NSString *errorMsg) {
+        NSLog(@"%@",errorMsg);
+    }];
+
 }
 
 /*
